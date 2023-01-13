9a28b04e27d27e0b302e21866108080dd9a141f2
gladys2811 <348629502@gapps.yrdsb.ca>
2023-01-13 14:44:10 -0500

    move left and right

--- Turtle.java
+++ Turtle.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+      import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Turtle.
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Turtle extends Actor
 {
+    String facing = "up";
+    
     /**
      * Act - do whatever the Turtle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,6 +17,21 @@ public class Turtle extends Actor
     public void act() 
     {
         // Add your action code here.
-        move(1);
+        move();
     }    
+    
+    public void move()
+    {
+        int x = getX();
+        int y = getY();
+        if (Greenfoot.isKeyDown("A"))
+        {
+            x -= 2;
+        }
+        if (Greenfoot.isKeyDown("D"))
+        {
+            x += 2;
+        }
+    }
+    
 }
