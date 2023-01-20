7d4ecc55038967471c59382ec1451d3d88d26701
gladys2811 <348629502@gapps.yrdsb.ca>
2023-01-20 06:32:06 -0500

    ninja jumps when space bar is pressed

--- Ninja.java
+++ Ninja.java
@@ -8,17 +8,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Ninja extends Actor
 {
-    
+    private final int GRAVITY = 1;
+    private int velocity;
     /**
      * Act - do whatever the Ninja wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public Ninja()
     {
-        
+        velocity = 0;
     }
     public void act() 
     {
-        
-    }    
+        fall();
+        if(Greenfoot.isKeyDown("space") && getY() > getWorld().getHeight() - 30) jump();
+    }
+    public void fall()
+    {
+        setLocation(getX(), getY() + velocity);
+        if (getY() > getWorld().getHeight() - 30) velocity = 0;
+        else velocity += GRAVITY;
+    }
+    public void jump()
+    {
+        velocity = - 15;
+    }
+    
+    
 }
